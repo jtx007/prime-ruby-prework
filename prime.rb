3993b03c7a23093de54ac54565ba5013a50b4b39
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-03 03:00:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@
 
 def prime?(integer)
   return false if integer < 2
-(2..integer - 1).any? {|x| integer % x == 0}
-  
-    false
+(2..integer - 1).each do |x|
+  if integer % x == 0
+   return false
   end
 end
 true
